4c03bca7b05b2f457932f446f98dfead14164087
LameIkon <troelsjelsbak@hotmail.com>
2023-11-14 15:43:33 +0100

    Zombie after food

--- Assets/_Complete-Game/Scripts/Enemy.cs
+++ Assets/_Complete-Game/Scripts/Enemy.cs
@@ -8,13 +8,21 @@ namespace Completed
 	{
 		public int playerDamage; 							//The amount of food points to subtract from the player when attacking.
 		public AudioClip attackSound1;						//First of two audio clips to play when attacking the player.
-		public AudioClip attackSound2;						//Second of two audio clips to play when attacking the player.
-		
-		
-		private Animator animator;							//Variable of type Animator to store a reference to the enemy's Animator component.
+		public AudioClip attackSound2;                      //Second of two audio clips to play when attacking the player.
+
+        public AudioClip eatSound1;                 //1 of 2 Audio clips to play when player collects a food object.
+        public AudioClip eatSound2;                 //2 of 2 Audio clips to play when player collects a food object.
+        public AudioClip drinkSound1;               //1 of 2 Audio clips to play when player collects a soda object.
+        public AudioClip drinkSound2;
+
+        private Animator animator;							//Variable of type Animator to store a reference to the enemy's Animator component.
 		private Transform target;							//Transform to attempt to move toward each turn.
-		private bool skipMove;								//Boolean to determine whether or not enemy should skip a turn or move this turn.
-		
+		private bool skipMove;                              //Boolean to determine whether or not enemy should skip a turn or move this turn.
+
+		//private List<Transform> targets;
+
+		//private Transform targetFood;
+		//private Transform targetSoda;
 		
 		//Start overrides the virtual Start function of the base class.
 		protected override void Start ()
@@ -25,13 +33,43 @@ namespace Completed
 			
 			//Get and store a reference to the attached Animator component.
 			animator = GetComponent<Animator> ();
-			
+
+			//target = new Transform[3];
+
+			SetTargetToClosestFood();
+
 			//Find the Player GameObject using it's tag and store a reference to its transform component.
-			target = GameObject.FindGameObjectWithTag ("Player").transform;
-			
+			//target = GameObject.FindGameObjectWithTag("Player").transform;
+
+		
 			//Call the start function of our base class MovingObject.
 			base.Start ();
 		}
+
+		void SetTargetToClosestFood() 
+		{
+            GameObject[] foods = GameObject.FindGameObjectsWithTag("Food");
+
+			if (foods.Length == 0)
+			{
+				target = GameObject.FindGameObjectWithTag("Player").transform;
+
+			}
+			else
+			{
+				target = foods[0].transform;
+
+				for (int i = 0; i < foods.Length; i++)
+				{
+
+					if (Vector2.Distance(transform.position, foods[i].transform.position) < Vector2.Distance(transform.position, target.position))
+					{
+						target = foods[i].transform;
+					}
+
+				}
+			}
+        }
 		
 		
 		//Override the AttemptMove function of MovingObject to include functionality needed for Enemy to skip turns.
@@ -57,24 +95,33 @@ namespace Completed
 		//MoveEnemy is called by the GameManger each turn to tell each Enemy to try to move towards the player.
 		public void MoveEnemy ()
 		{
+
+
+
+
 			//Declare variables for X and Y axis move directions, these range from -1 to 1.
 			//These values allow us to choose between the cardinal directions: up, down, left and right.
 			int xDir = 0;
 			int yDir = 0;
+
 			
-			//If the difference in positions is approximately zero (Epsilon) do the following:
-			if(Mathf.Abs (target.position.x - transform.position.x) < float.Epsilon)
+					//If the difference in positions is approximately zero (Epsilon) do the following:
+					if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+
+						//If the y coordinate of the target's (player) position is greater than the y coordinate of this enemy's position set y direction 1 (to move up). If not, set it to -1 (to move down).
+						yDir = target.position.y > transform.position.y ? 1 : -1;
+
+					//If the difference in positions is not approximately zero (Epsilon) do the following:
+					else
+						//Check if target x position is greater than enemy's x position, if so set x direction to 1 (move right), if not set to -1 (move left).
+						xDir = target.position.x > transform.position.x ? 1 : -1;
 				
-				//If the y coordinate of the target's (player) position is greater than the y coordinate of this enemy's position set y direction 1 (to move up). If not, set it to -1 (to move down).
-				yDir = target.position.y > transform.position.y ? 1 : -1;
 			
-			//If the difference in positions is not approximately zero (Epsilon) do the following:
-			else
-				//Check if target x position is greater than enemy's x position, if so set x direction to 1 (move right), if not set to -1 (move left).
-				xDir = target.position.x > transform.position.x ? 1 : -1;
+			
 			
 			//Call the AttemptMove function and pass in the generic parameter Player, because Enemy is moving and expecting to potentially encounter a Player
 			AttemptMove <Player> (xDir, yDir);
+
 		}
 		
 		
@@ -94,5 +141,24 @@ namespace Completed
 			//Call the RandomizeSfx function of SoundManager passing in the two audio clips to choose randomly between.
 			SoundManager.instance.RandomizeSfx (attackSound1, attackSound2);
 		}
-	}
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+			if (other.tag == "Food")
+            {
+                SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+
+                other.gameObject.SetActive(false);
+
+				SetTargetToClosestFood();
+            }
+
+            else if (other.tag == "Soda")
+            {
+				SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
+
+                other.gameObject.SetActive(false);
+            }
+        }
+    }
 }
